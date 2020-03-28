869ece58168c0f659a5e20ab08112a75579b87b6
Jitender Kumar <jitender.kumar@irissoftware.com>
2020-03-28 18:02:56 +0530

    Added authorization using configure with HttpSecurity

--- spring-boot-security/src/main/java/com/javabrains/security/springbootsecurity/SecurityConfiguration.java
+++ spring-boot-security/src/main/java/com/javabrains/security/springbootsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.javabrains.security.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,11 +17,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication()
 				.withUser("user")
 				.password("user")
-				.roles("USER");
+				.roles("USER")
+				.and()
+				.withUser("admin")
+				.password("admin")
+				.roles("ADMIN");
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//Configuration should be done in Most restrictive to least restrictive manner
+		http.authorizeRequests()
+				.antMatchers("/admin").hasRole("ADMIN")
+				.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/*").permitAll() //Any URL
+				.and().formLogin();
 	}
 }
