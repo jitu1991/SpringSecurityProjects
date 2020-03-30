681fc573996ebdfa2aea8221cc62b5a154994aa0
Jitender Kumar <jitender.kumar@irissoftware.com>
2020-03-30 14:06:57 +0530

    Spring security with jdbc using embedded db

---
+++ spring-security-jdbc/src/main/java/com/javabrains/security/springsecurityjdbc/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.javabrains.security.springsecurityjdbc;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)//Configure default with h2 embedded db
+		.withDefaultSchema()
+		.withUser(
+				User.withUsername("user")
+				.password("user")
+				.roles("USER")
+				)
+		.withUser(
+				User.withUsername("admin")
+				.password("admin")
+				.roles("ADMIN")
+				);
+		
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/").permitAll() //Any URL
+		.and().formLogin();
+	}
+}
